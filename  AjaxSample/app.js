aa6684d66fb4cf0a7a08a97a59d9a110c1a82410
ShoIwasaki <>
2021-01-13 23:50:42 +0900

    外部からのデータ取り込みソース追加

---  AjaxSample/app.js
+++  AjaxSample/app.js
@@ -1,39 +1,27 @@
-let imageList = [
-    {
-        "path":"img/img01.jpg",
-        "name":"sansaisan",
-        "caption":"こっちは貫禄ありすぎ"
-    },
-    {
-        "path":"img/img02.jpg",
-        "name":"yukky_13dream",
-        "caption":"年明け富士山"
-    },
-    {
-        "path":"img/img03.jpg",
-        "name":"maako",
-        "caption":"空と大地の境界線って、どのあたり？"
+let ajax = new XMLHttpRequest();
+ajax.open('GET', 'https://h2o-space.com/htmlbook/images.php');
+ajax.responseType = 'json';
+ajax.send(null);
+
+ajax.onreadystatechange = function () {
+    if (ajax.readyState === XMLHttpRequest.DONE && ajax.status === 200) {
+        for (let i = 0; i < this.response.length; i++) {
+
+            let data = this.response[i];
+            img = document.createElement('img');
+            img.setAttribute('src', data.path);
+
+            caption = document.createElement('div');
+            caption.className = 'inner';
+            caption.innerHTML = '<p>' + data.caption + '<span>' + data.name + '</span></p>';
+
+            div = document.createElement('div');
+            div.className = 'photo';
+            div.appendChild(img);
+            div.appendChild(caption);
+
+            document.getElementById('img_unit').appendChild(div);
+
+        }
     }
-];
-
-let img;
-let caption;
-let div;
-
-for (let i = 0; i < imageList.length; i++) {
-
-img = document.createElement('img');
-img.setAttribute('src',imageList[i].path);
-
-caption = document.createElement('div');
-caption.className = 'inner';
-caption.innerHTML = '<p>' + imageList[i].caption + '<span>' + imageList[i].name + '</span></p>';
-
-div = document.createElement('div');
-div.className = 'photo';
-div.appendChild(img);
-div.appendChild(caption);
-
-document.getElementById('img_unit').appendChild(div);
-
-}
+};
