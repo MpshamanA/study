98386c5f1118fb9045522706ae3ad87216b434fc
ShoIwasaki <>
2021-01-16 22:42:07 +0900

    console.logを削除

--- jQuerySample/js/app.js
+++ jQuerySample/js/app.js
@@ -1,6 +1,5 @@
 $.getJSON('https://h2o-space.com/htmlbook/images.php', function(data){
     for(var i = 0; i < data.length; i++){
-        console.log(data);
         $('<div class = "photo"></div>')
         .append('<img src = "' + data[i].path + '">')
         .append('<div class = "inner"><p>' + data[i].caption + '<span>' + data[i].name + '</span></p></div>')
